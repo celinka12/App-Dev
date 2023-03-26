86fb5497116a0b2ea817e6928b0c8fc3d64ec413
celinka12 <127372089+celinka12@users.noreply.github.com>
2023-03-26 22:31:23 +0700

    THA_W4_CELINKA E

---
+++ THA_W4_CELINKA_E/tolong.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace THA_W4_CELINKA_E
+{
+    internal class tolong
+    {
+
+    }
+}
