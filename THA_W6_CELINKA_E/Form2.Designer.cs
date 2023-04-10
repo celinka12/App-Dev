b5da8a85c553a3938bb1d54a9089b55450fb9f94
celinka12 <127372089+celinka12@users.noreply.github.com>
2023-04-10 08:44:24 +0700

    Add files via upload

---
+++ THA_W6_CELINKA_E/Form2.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace THA_W6_CELINKA_E
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_enter = new System.Windows.Forms.Button();
+            this.btn_delete = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_enter
+            // 
+            this.btn_enter.Location = new System.Drawing.Point(600, 371);
+            this.btn_enter.Name = "btn_enter";
+            this.btn_enter.Size = new System.Drawing.Size(130, 73);
+            this.btn_enter.TabIndex = 0;
+            this.btn_enter.Text = "Enter";
+            this.btn_enter.UseVisualStyleBackColor = true;
+            this.btn_enter.Click += new System.EventHandler(this.btn_enter_Click);
+            // 
+            // btn_delete
+            // 
+            this.btn_delete.Location = new System.Drawing.Point(1360, 371);
+            this.btn_delete.Name = "btn_delete";
+            this.btn_delete.Size = new System.Drawing.Size(125, 73);
+            this.btn_delete.TabIndex = 1;
+            this.btn_delete.Text = "Delete";
+            this.btn_delete.UseVisualStyleBackColor = true;
+            this.btn_delete.Click += new System.EventHandler(this.btn_delete_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1633, 820);
+            this.Controls.Add(this.btn_delete);
+            this.Controls.Add(this.btn_enter);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_enter;
+        private System.Windows.Forms.Button btn_delete;
+    }
+}
---
+++ THA_W6_CELINKA_E/Form2.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace THA_W6_CELINKA_E
+{
+    public partial class Form2 : Form
+    {
+        Form1 form = new Form1();
+        List<Button> buttonlist = new List<Button>();
+        List<Button> buttonlist2 = new List<Button>();
+        List<string> huruf = new List<string>() {"Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "A", "S", "D", "F", "G", "H", "J", "K", "L", "Z", "X", "C", "V", "B", "N", "M"};
+        string[] list = File.ReadAllText("Wordle Word List.txt").Split(',');
+        List<string> kata = new List<string>();
+        string isi = "";
+        List<char> katadobel = new List<char>();
+        int b = 0;
+        int c = 0;
+        int nambah = 0;
+        int plus = 10;
+        int baru = 0;
+        int yaa = 4;
+        int capek = 4;
+        int yaya = 0;
+        int cek = 0;
+        int neww = 0;
+        int baruu = 5;
+        int lain = 0;
+        string word = "";
+        int count = 0;
+        int dahla = 0;
+  
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < Form1.input; i++)
+            {
+                for (int a = 0; a < 5; a++)
+                {
+                    Button button = new Button();
+                    button.Tag = "tombol" + nambah;
+                    button.Size = new Size(50, 50);
+                    button.Location = new Point(20 + b, 40 + c);
+                    buttonlist.Add(button);
+                    nambah++;
+                    b += 50;
+                }
+                b = 0;
+                c += 60;
+            }
+            foreach (Button button in buttonlist)
+            {
+                this.Controls.Add(button);
+            }
+
+            nambah = 0;
+            b = 0;
+            c = 0;
+            for (int i = 0; i < 3; i++)
+            {
+                if (i == 2)
+                {
+                    plus -= 1;
+                    b += 40;
+                }
+                for (int a = 0; a < plus; a++)
+                {
+                    Button angka = new Button();
+                    angka.Tag = huruf[nambah];
+                    angka.Text = huruf[nambah];
+                    angka.Size = new Size(45, 45);
+                    angka.Location = new Point(300 + b, 100 + c);
+                    buttonlist2.Add(angka);
+                    nambah++;
+                    b += 45;
+                }
+                b = 0;
+                b += 25;
+                c += 45;
+                plus--;
+            }
+            foreach (Button angka in buttonlist2)
+            {
+                this.Controls.Add(angka);
+                angka.Click += Angka_Click;
+            }
+
+            string tebak = list[new Random().Next(0, list.Length)].ToUpper();
+            MessageBox.Show(tebak);
+            isi = tebak;
+        }
+
+        private void Angka_Click(object sender, EventArgs e)
+        {
+            if (buttonlist[yaa].Text == "")
+            {
+                Button huruf = sender as Button;
+                foreach (Button angka in buttonlist2)
+                {
+                    if (huruf.Tag == angka.Tag)
+                    {
+                        buttonlist[baru].Text = angka.Text;
+                    }
+                }
+                baru++;
+            }
+        }
+
+        private void btn_enter_Click(object sender, EventArgs e)
+        {
+            yaa += 5;
+            //for (int i = baruu - 5; i < baruu; i++)
+            //{
+            //    word += buttonlist[i].Text.ToUpper();
+            //}
+
+            for (int q = dahla; q < dahla + 5; q++)
+            {
+                Button nyoba = buttonlist[q];
+                if (nyoba.Text == "" || nyoba.Text == "")
+                {
+                    lain = 1;
+                }
+                else
+                {
+                    word += nyoba.Text;
+                    //for (int i = baruu - 5; i < baruu; i++)
+                    //{
+                    //    word += buttonlist[i].Text.ToUpper();
+                    //}
+                }
+            }
+            for (int z = 0; z < list.Length; z++)
+            {
+                if (word == list[z].ToUpper())
+                {
+                    count = 1;
+                }
+            }
+              
+            if (lain == 1)
+            {
+                MessageBox.Show("Please enter 5 letter word!");
+                word = "";
+                lain = 0;
+            }
+            else if (count == 0)
+            {
+                MessageBox.Show(word + " is not found");
+                word = "";
+            }
+            else
+            {
+                foreach (var F in isi)
+                {
+                    int charCount = 0;
+                    foreach (var chars in isi)
+                    {
+                        if (F == chars)
+                        {
+                            charCount++;
+                        }
+                        if (charCount > 1 && !katadobel.Contains(F))
+                        {
+                            katadobel.Add(F);
+                        }
+                    }
+                }
+
+                for (int a = 0; a < 5; a++)
+                {
+                    kata.Add(isi[a].ToString().ToUpper());
+                }
+
+                yaya = 0;
+                for (int z = lain; z <= capek; z++)
+                {
+                    if (buttonlist[z].Text == isi[yaya].ToString().ToUpper())
+                    {
+                        buttonlist[z].BackColor = Color.LightGreen;
+                        kata[yaya] = "";
+                        cek++;
+                    }
+                    yaya++;
+                }
+
+                for (int y = neww; y < baruu; y++)
+                {
+                    foreach (string kataa in kata)
+                    {
+                        if (buttonlist[y].Text == kataa.ToUpper())
+                        {
+                            buttonlist[y].BackColor = Color.Yellow;
+                            cek++;
+                        }
+                    }
+                }
+                count = 0;
+
+                if (word == isi)
+                {
+                    MessageBox.Show("Horee!");
+                    this.Close();
+                }
+               else if (dahla == 5 * Form1.input - 5)
+                {
+                    MessageBox.Show("Sadd!, Answer is" + isi);
+                    this.Close();
+                }
+                baruu += 5;
+            }
+            word = "";
+            neww += 5; 
+        }         
+        
+
+        private void btn_delete_Click(object sender, EventArgs e)
+        {
+            if (buttonlist[0].Text != "")
+            {
+                buttonlist[baru - 1].Text = "";
+                baru--;
+            }
+        }
+    }
+}
+    
