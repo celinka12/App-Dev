8a421a2c86ddf42068d36b25bfb8141e0512d576
celinka12 <127372089+celinka12@users.noreply.github.com>
2023-04-13 13:22:04 +0700

    CA_W8_celinnnnnnnn

---
+++ CA_W8/CA_W8/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace CA_W8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgv_Pemain = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelValue = new System.Windows.Forms.Label();
+            this.combo_NAtionality = new System.Windows.Forms.ComboBox();
+            this.combo_Country = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_Pemain)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgv_Pemain
+            // 
+            this.dgv_Pemain.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_Pemain.Location = new System.Drawing.Point(45, 68);
+            this.dgv_Pemain.Name = "dgv_Pemain";
+            this.dgv_Pemain.Size = new System.Drawing.Size(730, 344);
+            this.dgv_Pemain.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(42, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Value member";
+            // 
+            // labelValue
+            // 
+            this.labelValue.AutoSize = true;
+            this.labelValue.Location = new System.Drawing.Point(122, 39);
+            this.labelValue.Name = "labelValue";
+            this.labelValue.Size = new System.Drawing.Size(37, 13);
+            this.labelValue.TabIndex = 2;
+            this.labelValue.Text = "HEHE";
+            // 
+            // combo_NAtionality
+            // 
+            this.combo_NAtionality.FormattingEnabled = true;
+            this.combo_NAtionality.Location = new System.Drawing.Point(45, 13);
+            this.combo_NAtionality.Name = "combo_NAtionality";
+            this.combo_NAtionality.Size = new System.Drawing.Size(121, 21);
+            this.combo_NAtionality.TabIndex = 3;
+            this.combo_NAtionality.SelectedIndexChanged += new System.EventHandler(this.combo_NAtionality_SelectedIndexChanged);
+            // 
+            // combo_Country
+            // 
+            this.combo_Country.FormattingEnabled = true;
+            this.combo_Country.Location = new System.Drawing.Point(711, 12);
+            this.combo_Country.Name = "combo_Country";
+            this.combo_Country.Size = new System.Drawing.Size(121, 21);
+            this.combo_Country.TabIndex = 4;
+            this.combo_Country.SelectedIndexChanged += new System.EventHandler(this.combo_Country_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(861, 424);
+            this.Controls.Add(this.combo_Country);
+            this.Controls.Add(this.combo_NAtionality);
+            this.Controls.Add(this.labelValue);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dgv_Pemain);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_Pemain)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgv_Pemain;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelValue;
+        private System.Windows.Forms.ComboBox combo_NAtionality;
+        private System.Windows.Forms.ComboBox combo_Country;
+    }
+}
+
---
+++ CA_W8/CA_W8/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace CA_W8
+{
+    public partial class Form1 : Form
+    {
+        MySqlConnection sqlConnect;
+        MySqlCommand sqlCommand; // menerima perintah query // select*form itu
+        MySqlDataAdapter SQLAdapter; // menerima hasil dari querynya // hasil dari selectform 
+        string connectionString;
+        string sqlQuery; // cm buat nulis querynya
+        DataTable dtPemain = new DataTable();
+        DataTable dtNationality = new DataTable();
+        DataTable dtCountry = new DataTable();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            connectionString = "server=localhost;uid=root;pwd=;database=premier_league;"; // pwd di isi pwd sndiri
+            sqlConnect = new MySqlConnection(connectionString);
+            sqlConnect.Open();  //hanya digunakan ketika kalian DML , insert,update,delete . jk tidak maka gausah karna program bisa jadi berat  . Pastikan sudh g crash pas sql open di pct
+
+            //sqlQuery = "SELECT player_id as 'Player ID',\r\nPlayer_name as 'Player Name',\r\nheight  as 'Player Height'\r\nfrom player;";
+            //sqlCommand = new MySqlCommand (sqlQuery, sqlConnect);
+            //SQLAdapter = new MySqlDataAdapter(sqlCommand);
+            //SQLAdapter.Fill(dtPemain);
+            //dgv_Pemain.DataSource = dtPemain;
+          
+
+            //sqlQuery = "select abv as `Abrevation`, nation as `Nation` from nationality;";
+            //sqlCommand = new MySqlCommand (sqlQuery, sqlConnect);
+            //SQLAdapter = new MySqlDataAdapter( sqlCommand); 
+            //SQLAdapter.Fill(dtNationality);
+            //combo_NAtionality.DataSource = dtNationality;
+            //combo_NAtionality.ValueMember = "Abrevation"; 
+            //combo_NAtionality.DisplayMember = "Nation";
+
+            sqlQuery = "SELECT team_name, team_id from team;";
+            sqlCommand = new MySqlCommand(sqlQuery, sqlConnect);
+            SQLAdapter = new MySqlDataAdapter(sqlCommand);
+            SQLAdapter.Fill(dtPemain);
+            combo_Country.DataSource = dtPemain;
+            combo_Country.ValueMember = "Team_name";
+            combo_Country.DisplayMember = "Team_name";
+         
+
+        }
+
+        private void combo_NAtionality_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            labelValue.Text = combo_NAtionality.SelectedValue.ToString();
+        }
+
+        private void combo_Country_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            dtCountry = new DataTable();
+            dtCountry.Clear();
+          //  string save = combo_Country.SelectedValue.ToString();
+           // sqlQuery = $"select player_id, player_name, height, team_name from player inner join team on player.team_id = team.team_id where team_name ='{save}';";
+            sqlQuery = "select player_id, player_name, height, team_name from player inner join team on player.team_id = team.team_id where team_name ='" + combo_Country.SelectedValue.ToString()+"';";
+            sqlCommand = new MySqlCommand(sqlQuery, sqlConnect);
+            SQLAdapter = new MySqlDataAdapter(sqlCommand);
+            SQLAdapter.Fill(dtCountry);
+          
+            dgv_Pemain.DataSource = dtCountry;
+
+        }
+    }
+}
