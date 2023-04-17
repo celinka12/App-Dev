5e29545b2bd64acba3a21dc86a3b105557070317
celinka12 <127372089+celinka12@users.noreply.github.com>
2023-04-17 12:51:48 +0700

    CA_W8

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menustrip = new System.Windows.Forms.MenuStrip();
+            this.teamDataToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.findMatchToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cb_data = new System.Windows.Forms.ComboBox();
+            this.dgv_data = new System.Windows.Forms.DataGridView();
+            this.dgv_match = new System.Windows.Forms.DataGridView();
+            this.menustrip.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_data)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_match)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menustrip
+            // 
+            this.menustrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.teamDataToolStripMenuItem,
+            this.findMatchToolStripMenuItem});
+            this.menustrip.Location = new System.Drawing.Point(0, 0);
+            this.menustrip.Name = "menustrip";
+            this.menustrip.Size = new System.Drawing.Size(800, 24);
+            this.menustrip.TabIndex = 0;
+            this.menustrip.Text = "dtPemain";
+            // 
+            // teamDataToolStripMenuItem
+            // 
+            this.teamDataToolStripMenuItem.Name = "teamDataToolStripMenuItem";
+            this.teamDataToolStripMenuItem.Size = new System.Drawing.Size(74, 20);
+            this.teamDataToolStripMenuItem.Text = "Team Data";
+            this.teamDataToolStripMenuItem.Click += new System.EventHandler(this.teamDataToolStripMenuItem_Click);
+            // 
+            // findMatchToolStripMenuItem
+            // 
+            this.findMatchToolStripMenuItem.Name = "findMatchToolStripMenuItem";
+            this.findMatchToolStripMenuItem.Size = new System.Drawing.Size(79, 20);
+            this.findMatchToolStripMenuItem.Text = "Find Match";
+            this.findMatchToolStripMenuItem.Click += new System.EventHandler(this.findMatchToolStripMenuItem_Click);
+            // 
+            // cb_data
+            // 
+            this.cb_data.FormattingEnabled = true;
+            this.cb_data.Location = new System.Drawing.Point(13, 40);
+            this.cb_data.Name = "cb_data";
+            this.cb_data.Size = new System.Drawing.Size(121, 21);
+            this.cb_data.TabIndex = 1;
+            this.cb_data.SelectedIndexChanged += new System.EventHandler(this.cb_data_SelectedIndexChanged);
+            // 
+            // dgv_data
+            // 
+            this.dgv_data.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_data.Location = new System.Drawing.Point(32, 84);
+            this.dgv_data.Name = "dgv_data";
+            this.dgv_data.Size = new System.Drawing.Size(738, 150);
+            this.dgv_data.TabIndex = 2;
+            // 
+            // dgv_match
+            // 
+            this.dgv_match.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_match.Location = new System.Drawing.Point(32, 240);
+            this.dgv_match.Name = "dgv_match";
+            this.dgv_match.Size = new System.Drawing.Size(738, 150);
+            this.dgv_match.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 399);
+            this.Controls.Add(this.dgv_match);
+            this.Controls.Add(this.dgv_data);
+            this.Controls.Add(this.cb_data);
+            this.Controls.Add(this.menustrip);
+            this.MainMenuStrip = this.menustrip;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menustrip.ResumeLayout(false);
+            this.menustrip.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_data)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_match)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menustrip;
+        private System.Windows.Forms.ToolStripMenuItem teamDataToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem findMatchToolStripMenuItem;
+        private System.Windows.Forms.ComboBox cb_data;
+        private System.Windows.Forms.DataGridView dgv_data;
+        private System.Windows.Forms.DataGridView dgv_match;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        MySqlConnection sqlConnect;
+        MySqlCommand sqlCommand; // menerima perintah query // select*form itu
+        MySqlDataAdapter SQLAdapter;
+        string connectionString;
+        string sqlQuery;
+        DataTable dtPemain = new DataTable();
+        DataTable dtCountry = new DataTable();
+        DataTable dtmanager = new DataTable(); 
+        DataTable dtmatch = new DataTable();    
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            connectionString = "server=localhost;uid=root;pwd=;database=premier_league;";
+            sqlConnect = new MySqlConnection(connectionString);
+            sqlConnect.Open();
+
+            sqlQuery = "SELECT team_name, team_id from team;";
+            sqlCommand = new MySqlCommand(sqlQuery, sqlConnect);
+            SQLAdapter = new MySqlDataAdapter(sqlCommand);
+            SQLAdapter.Fill(dtPemain);
+            cb_data.ValueMember = "Team_name";
+            cb_data.DisplayMember = "Team_id";
+            cb_data.DataSource = dtPemain;
+        }
+
+        private void cb_data_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //dgv_match.Visible = false;
+            dtCountry.Clear();  
+            string save = cb_data.SelectedValue.ToString();
+            sqlQuery = $"select player_name, team_name, weight, height from player inner join team on player.team_id = team.team_id where team_name ='{save}';";
+            sqlCommand = new MySqlCommand(sqlQuery, sqlConnect);
+            SQLAdapter = new MySqlDataAdapter(sqlCommand);
+            SQLAdapter.Fill(dtCountry);
+            dgv_data.DataSource = dtCountry;
+
+
+            dtmanager.Clear();
+            sqlQuery = $"select team_name, manager_name, birthdate, nation from manager inner join team on manager.manager_id = team.manager_id inner join nationality on manager.nationality_id = nationality.nationality_id where team_name = '{save}';";
+            sqlCommand = new MySqlCommand(sqlQuery, sqlConnect);
+            SQLAdapter = new MySqlDataAdapter(sqlCommand);
+            SQLAdapter.Fill(dtmanager);
+            dgv_match.DataSource = dtmanager;
+        }
+
+        private void findMatchToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+           // dgv_match.Visible = false;
+            dtmatch.Clear();
+            string savee = cb_data.SelectedValue.ToString();
+            sqlQuery = $"select team_name, manager_name, birthdate, nation from manager inner join team on manager.manager_id = team.manager_id inner join nationality on manager.nationality_id = nationality.nationality_id where team_name = '{savee}';";
+            sqlCommand = new MySqlCommand(sqlQuery, sqlConnect);
+            SQLAdapter = new MySqlDataAdapter(sqlCommand);
+            SQLAdapter.Fill(dtmatch);
+            dgv_match.DataSource = dtmatch;
+        }
+
+        private void teamDataToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
